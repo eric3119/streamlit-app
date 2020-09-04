7720c1e933761a2105e153f84d14298b6f81bd75
eric3119 <esc2@ic.ufal.br>
2020-09-04 17:26:58 -0300

    update

--- filtro.py
+++ filtro.py
@@ -1,28 +1,40 @@
 import re
 import pandas as pd
+import streamlit as st
 
-professor = pd.read_csv('data/TS_PROFESSOR_2017.csv')
-professor = professor.dropna()
+@st.cache
+def load_data(path):
+    data = pd.read_csv(path)
+    data = data.dropna()
+    return data
 
-municipios = pd.read_excel('data/TS_MUNICIPIO.xlsx')
+@st.cache
+def load_excel(path):
+    data = pd.read_excel(path)
+    data = data.dropna()
+    return data
+
+professor = load_data('data/TS_PROFESSOR_2017.csv')
+
+municipios = load_excel('data/TS_MUNICIPIO.xlsx')
 municipios = municipios.rename({'CO_MUNICIPIO': "ID_MUNICIPIO"}, axis="columns")
 
 municipios_alagoas = municipios[municipios['NO_UF'] == 'Alagoas']
 professor_alagoas = professor[professor['ID_UF'] == 27]
 
-municipio_professor = municipios_alagoas.merge(professor_alagoas)
+municipio_professor = professor_alagoas.merge(municipios_alagoas).dropna()
 
-mean_cols_MT = list(
-        filter(lambda x: re.match("nivel_.*_MT", str(x)), list(municipio_professor.columns))
-    )
-mean_cols_LP = list(
-        filter(lambda x: re.match("nivel_.*_LP", str(x)), list(municipio_professor.columns))
-    )
+# mean_cols_MT = list(
+#         filter(lambda x: re.match("nivel_.*_MT", str(x)), list(municipio_professor.columns))
+#     )
+# mean_cols_LP = list(
+#         filter(lambda x: re.match("nivel_.*_LP", str(x)), list(municipio_professor.columns))
+#     )
 
-MEDIA_MT = municipio_professor[mean_cols_MT].mean().mean()
-MEDIA_LP = municipio_professor[mean_cols_LP].mean().mean()
-municipio_professor['MEDIA_MT'] = municipio_professor[mean_cols_MT].mean(axis=1)
-municipio_professor['MEDIA_LP'] = municipio_professor[mean_cols_LP].mean(axis=1)
+MEDIA_MT = municipio_professor['MEDIA_5_MT'].mean()
+MEDIA_LP = municipio_professor['MEDIA_5_LP'].mean()
+# # municipio_professor['MEDIA_MT'] = municipio_professor[mean_cols_MT].mean(axis=1)
+# # municipio_professor['MEDIA_LP'] = municipio_professor[mean_cols_LP].mean(axis=1)
 
 def get_rank(el, mean):
     if(el > mean):
@@ -32,5 +44,5 @@ def get_rank(el, mean):
     else:
         return 'IGUAL A MEDIA'
 
-municipio_professor['MEDIA_MT'] = municipio_professor['MEDIA_MT'].apply(lambda x: get_rank(x, MEDIA_MT))
-municipio_professor['MEDIA_LP'] = municipio_professor['MEDIA_LP'].apply(lambda x: get_rank(x, MEDIA_LP))
+municipio_professor['MEDIA_MT'] = municipio_professor['MEDIA_5_MT'].apply(lambda x: get_rank(x, MEDIA_MT))
+municipio_professor['MEDIA_LP'] = municipio_professor['MEDIA_5_LP'].apply(lambda x: get_rank(x, MEDIA_LP))
--- main.py
+++ main.py
@@ -2,13 +2,25 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
+from sklearn.feature_selection import SelectKBest, VarianceThreshold
+from sklearn.feature_selection import f_classif, mutual_info_classif
+
 DATA_URL = 'data/TS_PROFESSOR_2017.csv'
+RES_MUNICIPIOS_URL = 'data/TS_MUNICIPIO.xlsx'
+MUNICIPIOS_URL = 'data/dicionario_municipios.csv'
 
 st.title(DATA_URL)
 
 @st.cache
 def load_data(path):
     data = pd.read_csv(path)
+    data = data.dropna()
+    lowercase = lambda x: str(x).lower()
+    data.rename(lowercase, axis='columns', inplace=True)
+    return data
+@st.cache
+def load_excel(path):
+    data = pd.read_excel(path)
     data = data.dropna()
     lowercase = lambda x: str(x).lower()
     data.rename(lowercase, axis='columns', inplace=True)
@@ -22,6 +34,49 @@ def format_municipio(el):
 
 def format_uf(el):
     return DICIONARIO['id_uf'][str(el)]
+
+def format_serie(el):
+    return DICIONARIO['id_serie'][el]
+
+def get_rank(el, mean):
+    if(el > mean):
+        return 1#'ACIMA DA MEDIA'
+    elif(el < mean):
+        return 2#'ABAIXO DA MEDIA'
+    else:
+        return 3#'IGUAL A MEDIA'
+
+def classify(X, y, text):
+    kbest = SelectKBest(score_func=f_classif, k=4)
+    fit = kbest.fit(X,y)
+    features = fit.transform(X)
+
+    # Visualizando as features:
+    st.write(text)
+    st.write(features)
+
+def clear_variance(X):
+    constant_filter = VarianceThreshold(threshold=0)
+    constant_filter.fit(X)
+    constant_columns = [column for column in X.columns if column not in X.columns[constant_filter.get_support()]]
+    X_test = constant_filter.transform(X)
+    
+    st.write("Removed columns VarianceThreshold(threshold=0)")
+    st.write(constant_columns)
+    
+    return X[X.columns[constant_filter.get_support()]]
+
+# Create a text element and let the reader know the data is loading.
+data_load_state = st.text('Loading data...')
+
+data = load_data(DATA_URL)
+municipios = load_data(MUNICIPIOS_URL)
+
+res_municipios = load_excel(RES_MUNICIPIOS_URL)
+res_municipios = res_municipios.rename({'co_municipio': "id_municipio"}, axis="columns")
+# Notify the reader that the data was successfully loaded.
+data_load_state.text("Done! (using st.cache)")
+
 
 DICIONARIO = {
     "id_uf":{
@@ -53,11 +108,11 @@ DICIONARIO = {
         "52": "GO",
         "53": "DF",
     },
-    "id_municipio": load_data('data/dicionario_municipios.csv').set_index('co_municipio').to_dict()['no_municipio'],
+    "id_municipio": municipios.set_index('co_municipio').to_dict()['no_municipio'],
     "id_serie":{
-        "5": "4ª série/5º ano EF",
-        "9": "8ª série/9º ano EF",
-        "12": "3ª Série do Ensino Médio",
+        5: "4ª série/5º ano EF",
+        9: "8ª série/9º ano EF",
+        12: "3ª Série do Ensino Médio",
     },
     "id_dependencia_adm":{
         "1": "Federal",
@@ -75,21 +130,15 @@ DICIONARIO = {
     }
 }
 
-# Create a text element and let the reader know the data is loading.
-data_load_state = st.text('Loading data...')
-# Load 10,000 rows of data into the dataframe.
-data = load_data(DATA_URL)
-# Notify the reader that the data was successfully loaded.
-data_load_state.text("Done! (using st.cache)")
-
 # filters
 id_uf = st.sidebar.selectbox('UF', data['id_uf'].unique(), format_func=format_uf)
 id_municipio = st.sidebar.selectbox('Município', data[data['id_uf'] == id_uf]['id_municipio'].unique(), format_func=format_municipio)
+id_serie = st.sidebar.selectbox('Série', list(DICIONARIO['id_serie']), format_func=format_serie)
 
 rows = ([
         #'id_prova_brasil', 
-        #'id_uf', 
-        #'id_municipio', 
+        'id_uf', 
+        'id_municipio', 
         'id_escola',
         'id_dependencia_adm',
         'id_localizacao',
@@ -97,17 +146,33 @@ rows = ([
         'co_professor',
         'id_serie',
         'in_preenchimento_questionario'
-    ]# + [f'tx_resp_q{x:03d}' for x in range(1,126)]
+    ] + [f'tx_resp_q{x:03d}' for x in range(1,126)]
     )
 
 filtered_data = data[data['id_municipio'] == id_municipio][rows]
+filtered_data = filtered_data[filtered_data['id_serie'] == id_serie]
 
-st.subheader(f'Filtros - UF: {format_uf(id_uf)}, Município: {format_municipio(id_municipio)}')
+st.subheader(f'Filtros:\nUF: {format_uf(id_uf)}, Município: {format_municipio(id_municipio)}, Série: {format_serie(id_serie)}')
 st.write(f"{filtered_data['id_escola'].nunique()} escolas")
 if st.checkbox('Show raw data'):
     st.write(filtered_data)
     st.write(f'(linhas, colunas) = {filtered_data.shape}')
 
-#st.write(id_municipio)
+if st.checkbox('sklearn SelectKBest'):
+    res_municipios_filtered = res_municipios[res_municipios['id_municipio'] == id_municipio]
+    
+    municipio_professor = res_municipios_filtered.merge(filtered_data)
 
-#st.write(filtered_data['tx_resp_q001'].value_counts())
+    MEDIA_MT = municipio_professor['media_5_mt'].mean()
+    MEDIA_LP = municipio_professor['media_5_lp'].mean()
+    
+    X = municipio_professor[[f'tx_resp_q{x:03d}' for x in range(1,126)]]
+    X = X.apply(lambda x: list(map(ord, x)))
+    X = clear_variance(X)
+
+    y = municipio_professor['media_5_mt'].apply(lambda x: get_rank(x, MEDIA_MT))
+    classify(X,y, 'media_5_mt')
+    y = municipio_professor['media_5_lp'].apply(lambda x: get_rank(x, MEDIA_LP))
+    classify(X,y, 'media_5_lp')
+
+    
